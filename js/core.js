e79ccd08b1a3516c06928974f91e0c989c955c4d
Fairy <125118984+FairyHmm@users.noreply.github.com>
2023-11-11 22:13:03 +0700

    upload

--- js/core.js
+++ js/core.js
@@ -1,10 +1,10 @@
 // Nav
 function openNav() {
   var side = document.getElementById("mySidenav");
-  if (side.style.width == "250px") {
+  if (side.style.width == "200px") {
     side.style.width = "0px";
   } else {
-    side.style.width = "250px";
+    side.style.width = "200px";
   }
 }
 // Lang selector
