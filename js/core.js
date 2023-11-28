a77fc262848c40e4494573a61b3d7def30de7f79
Fairy <125118984+FairyHmm@users.noreply.github.com>
2023-11-28 23:00:32 +0700

    Update sprite, learn, and play

--- js/core.js
+++ js/core.js
@@ -7,21 +7,3 @@ function openNav() {
     side.style.width = "200px";
   }
 }
-// Lang selector
-var language;
-function getLanguage() {
-  localStorage.getItem("language") == null ? setLanguage("en") : false;
-  $.ajax({
-    url: "/language/" + localStorage.getItem("language") + ".json",
-    dataType: "json",
-    async: false,
-    dataType: "json",
-    success: function (lang) {
-      language = lang;
-    },
-  });
-}
-
-function setLanguage(lang) {
-  localStorage.setItem("language", lang);
-}
--- learn/learn.js
+++ learn/learn.js
@@ -1,34 +1,4 @@
-function toggleLearn() {
-  if (document.getElementById("quiz-list").classList.contains("show")) {
-    document.getElementById("quiz-list").classList.remove("show");
-    setTimeout(() => {
-      document.getElementById("learn-list").classList.toggle("show");
-    }, 1000);
-  } else {
-    document.getElementById("learn-list").classList.toggle("show");
-  }
+function toggleCards() {
+  var cards = document.getElementsByClassName("cards");
+  cards[0].style.opacity = "1";
 }
-
-function toggleQuiz() {
-  if (document.getElementById("learn-list").classList.contains("show")) {
-    document.getElementById("learn-list").classList.remove("show");
-    setTimeout(() => {
-      document.getElementById("quiz-list").classList.toggle("show");
-    }, 1000);
-  } else {
-    document.getElementById("quiz-list").classList.toggle("show");
-  }
-}
-
-window.onclick = function (event) {
-  if (!event.target.matches(".learn")) {
-    if (document.getElementById("learn-list").classList.contains("show")) {
-      document.getElementById("learn-list").classList.remove("show");
-    }
-  }
-  if (!event.target.matches(".quiz")) {
-    if (document.getElementById("quiz-list").classList.contains("show")) {
-      document.getElementById("quiz-list").classList.remove("show");
-    }
-  }
-};
